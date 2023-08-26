9661e97d5f7e2d67ec3190787fcc001ad897b1c9
Maxime Gagne <max.8989@hotmail.com>
2023-08-25 23:33:44 -0400

    get data into card

---
+++ src/components/animeCard.jsx
@@ -0,0 +1,25 @@
+const AnimeCard = ({ anime }) => {
+  return (
+    <div className='card lg:card-side bg-base-100 shadow-xl my-3'>
+      <figure className='w-1/4'>
+        <img src={anime.thumbnail} alt={anime.title} />
+      </figure>
+      <div className='card-body w-3/4'>
+        <h2 className='card-title'>{anime.title}</h2>
+        <p>{anime.shortDescription}</p>
+        <div className='card-actions justify-end'>
+          <a
+            className='btn btn-primary'
+            href={anime.video}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            MyAnimeList
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimeCard;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
+import AnimeCard from './animeCard';
+
 export default function Home() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [searchInput, setSearchInput] = useState('');
+  const [result, setResult] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      console.log(apiUrl);
+      const response = await axios.post(apiUrl, { search: searchInput });
+      if (response.data && Array.isArray(response.data)) {
+        setResult(response.data);
+      }
+    } catch (error) {
+      console.error('Error searching:', error);
+    }
+  };
+
   return (
     <div className='flex justify-center'>
-      <input
-        type='text'
-        placeholder='Type here'
-        className='input input-bordered input-info w-full max-w-xs'
-      />
+      <div className='flex justify-center flex-col items-center'>
+        <input
+          type='text'
+          placeholder='Type here'
+          className='input input-bordered input-info w-full max-w-xs'
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+            handleSearch();
+          }}
+        />
+        <div className='flex justify-center flex-col max-w-xl'>
+          {result.map((anime, index) => (
+            <AnimeCard key={index} anime={anime} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
