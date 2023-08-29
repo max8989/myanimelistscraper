d92bfd927fed56ead2ec0d62622aac2d6d5dd62e
Maxime Gagne <max.8989@hotmail.com>
2023-08-28 20:23:04 -0400

    update search

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,11 +3,13 @@ import axios from 'axios';
 import AnimeCard from './animeCard';
 import { AnimeTable } from './animetable';
 
+const DEFAULT_USERNAME = 'centricmax';
+
 const SearchTypes = {
-  Anime: "anime",
+  Anime: 'anime',
   //Manga: "manga",
-  WatchList: "watchlist",
-}
+  WatchList: 'watchlist',
+};
 
 export default function Home() {
   const apiUrl = import.meta.env.VITE_API_URL;
@@ -20,10 +22,9 @@ export default function Home() {
 
   const handleSearch = async () => {
     try {
-      if (searchType === SearchTypes.WatchList)
-        return;
+      if (searchType === SearchTypes.WatchList) return;
 
-      if (searchInput.length <= 4){
+      if (searchInput.length <= 4) {
         setResult([]);
         return;
       }
@@ -39,7 +40,7 @@ export default function Home() {
 
   const handleSearchWatchList = async () => {
     try {
-      if (searchInput.length <= 4){
+      if (searchInput.length <= 4) {
         setResult([]);
         return;
       }
@@ -51,7 +52,7 @@ export default function Home() {
       }
     } catch (error) {
       console.error('Error searching:', error);
-      setErrorMessage('No Account found!')
+      setErrorMessage('No Account found!');
     } finally {
       setSearchBtnLoading(false);
     }
@@ -59,9 +60,7 @@ export default function Home() {
 
   return (
     <div className='flex justify-center'>
-      
       <div className='flex justify-center flex-col items-center'>
-
         <div className='flex flex-row items-center mb-10 mt-3'>
           <input
             type='text'
@@ -75,16 +74,26 @@ export default function Home() {
             }}
           />
 
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className="btn m-1">{searchType}</label>
-            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-              {Object.keys(SearchTypes).map(type => (
-                <li key={type} onClick={() => {
-                   setSearchType(SearchTypes[type]); 
-                   setResult([]);
-                   setSearchInput('');
-                   setWatchListResult([]);
-                   }}>
+          <div className='dropdown dropdown-hover'>
+            <label tabIndex={0} className='btn m-1'>
+              {searchType}
+            </label>
+            <ul
+              tabIndex={0}
+              className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'
+            >
+              {Object.keys(SearchTypes).map((type) => (
+                <li
+                  key={type}
+                  onClick={() => {
+                    setSearchType(SearchTypes[type]);
+                    setResult([]);
+                    setSearchInput(
+                      SearchTypes[type] === SearchTypes.WatchList ? DEFAULT_USERNAME : '',
+                    );
+                    setWatchListResult([]);
+                  }}
+                >
                   <a>{type}</a>
                 </li>
               ))}
@@ -92,28 +101,38 @@ export default function Home() {
           </div>
 
           {searchType === SearchTypes.WatchList && !searchBtnLoading ? (
-            <button
-             className="btn btn-success"
-             onClick={() => handleSearchWatchList()}
-             >Search</button>
-          ) : null}
-
-          {searchType === SearchTypes.WatchList && searchBtnLoading ? (
-            <button className="btn">
-              <span className="loading loading-spinner"></span>
-              loading
+            <button className='btn btn-success' onClick={() => handleSearchWatchList()}>
+              Search
             </button>
           ) : null}
 
+          {searchType === SearchTypes.WatchList && searchBtnLoading ? (
+            <button className='btn'>
+              <span className='loading loading-spinner'></span>
+              loading
+            </button>
+          ) : null}
         </div>
-        
+
         {errorMessage ? (
-          <div className="alert alert-error">
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <div className='alert alert-error'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='stroke-current shrink-0 h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+              />
+            </svg>
             <span>{errorMessage}</span>
           </div>
         ) : null}
-        
+
         {searchType === SearchTypes.Anime ? (
           <div className='flex justify-center flex-col max-w-xl'>
             {result.map((anime, index) => (
