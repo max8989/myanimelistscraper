8ec66d3fb6c487fc00311141a80b9211af6d15fc
Maxime Gagne <max.8989@hotmail.com>
2023-08-29 00:36:41 -0400

    add basic animation

--- src/components/anime_table.jsx
+++ src/components/anime_table.jsx
@@ -1,24 +1,11 @@
-import { useState, useEffect } from 'react';
 import { AnimeTableRow } from './anime_table_row';
 
 export function AnimeTable({ data }) {
-  const [currentRowIndex, setCurrentRowIndex] = useState(0);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (currentRowIndex < data.length - 1) {
-        setCurrentRowIndex((prevIndex) => prevIndex + 1);
-      }
-    }, 150);
-
-    return () => clearTimeout(timer);
-  }, [currentRowIndex, data.length]);
-
   return (
-    <div className='overflow-x-auto'>
+    <div className='overflow-x-auto animate-fade-in'>
       <table className='table table-pin-rows table-pin-cols'>
         <tbody>
-          {data.slice(0, currentRowIndex + 1).map((anime, idx) => (
+          {data.map((anime, idx) => (
             <AnimeTableRow key={idx} anime={anime} idx={idx} />
           ))}
         </tbody>
