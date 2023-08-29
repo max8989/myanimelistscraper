ccfe16ee91e9e93d7f12bb8d5d5c7f6f90e16683
Maxime Gagne <max.8989@hotmail.com>
2023-08-28 20:06:11 -0400

    add watchlist

---
+++ src/components/animeTableRow.jsx
@@ -0,0 +1,37 @@
+export function AnimeTableRow({ anime, idx }) {
+    return (
+      <tr>
+        <th>
+          {idx + 1}
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={anime.animeImagePath} alt="Anime Image" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{anime.animeTitle}</div>
+              <div className="text-sm opacity-50">{anime.animeStartDateString} - {anime.animeEndDateString}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          {anime.animeStudios.map(studio => studio.name).join(', ')}
+          <br />
+          <span className="badge badge-ghost badge-sm">{anime.animeMediaTypeString}</span>
+        </td>
+        <td>{anime.genres.map(genre => genre.name).join(', ')}</td>
+        <th>
+          <a 
+          href={`https://myanimelist.net${anime.animeUrl}`} 
+          className="btn btn-ghost btn-xs"
+          target='_blank'
+          rel='noopener noreferrer'
+            >details</a>
+        </th>
+      </tr>
+    );
+  }
+  
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react';
 import axios from 'axios';
 import AnimeCard from './animeCard';
+import { AnimeTable } from './animetable';
+
+const SearchTypes = {
+  Anime: "anime",
+  //Manga: "manga",
+  WatchList: "watchlist",
+}
 
 export default function Home() {
   const apiUrl = import.meta.env.VITE_API_URL;
+  const [searchType, setSearchType] = useState(SearchTypes.Anime);
   const [searchInput, setSearchInput] = useState('');
+  const [searchBtnLoading, setSearchBtnLoading] = useState(false);
   const [result, setResult] = useState([]);
+  const [watchListResult, setWatchListResult] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSearch = async () => {
     try {
-      console.log(apiUrl);
-      const response = await axios.post(apiUrl, { search: searchInput });
+      if (searchType === SearchTypes.WatchList)
+        return;
+
+      if (searchInput.length <= 4){
+        setResult([]);
+        return;
+      }
+
+      const response = await axios.post(`${apiUrl}/search/${searchType}`, { search: searchInput });
       if (response.data && Array.isArray(response.data)) {
         setResult(response.data);
       }
@@ -19,24 +37,96 @@ export default function Home() {
     }
   };
 
+  const handleSearchWatchList = async () => {
+    try {
+      if (searchInput.length <= 4){
+        setResult([]);
+        return;
+      }
+      setSearchBtnLoading(true);
+
+      const response = await axios.post(`${apiUrl}/search/${searchType}`, { search: searchInput });
+      if (response.data && Array.isArray(response.data)) {
+        setWatchListResult(response.data);
+      }
+    } catch (error) {
+      console.error('Error searching:', error);
+      setErrorMessage('No Account found!')
+    } finally {
+      setSearchBtnLoading(false);
+    }
+  };
+
   return (
     <div className='flex justify-center'>
+      
       <div className='flex justify-center flex-col items-center'>
-        <input
-          type='text'
-          placeholder='Type here'
-          className='input input-bordered input-info w-full max-w-xs'
-          value={searchInput}
-          onChange={(e) => {
-            setSearchInput(e.target.value);
-            handleSearch();
-          }}
-        />
-        <div className='flex justify-center flex-col max-w-xl'>
-          {result.map((anime, index) => (
-            <AnimeCard key={index} anime={anime} />
-          ))}
+
+        <div className='flex flex-row items-center mb-10 mt-3'>
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input input-bordered input-info w-full max-w-xs'
+            value={searchInput}
+            onChange={(e) => {
+              setErrorMessage('');
+              setSearchInput(e.target.value);
+              handleSearch();
+            }}
+          />
+
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className="btn m-1">{searchType}</label>
+            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+              {Object.keys(SearchTypes).map(type => (
+                <li key={type} onClick={() => {
+                   setSearchType(SearchTypes[type]); 
+                   setResult([]);
+                   setSearchInput('');
+                   setWatchListResult([]);
+                   }}>
+                  <a>{type}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {searchType === SearchTypes.WatchList && !searchBtnLoading ? (
+            <button
+             className="btn btn-success"
+             onClick={() => handleSearchWatchList()}
+             >Search</button>
+          ) : null}
+
+          {searchType === SearchTypes.WatchList && searchBtnLoading ? (
+            <button className="btn">
+              <span className="loading loading-spinner"></span>
+              loading
+            </button>
+          ) : null}
+
         </div>
+        
+        {errorMessage ? (
+          <div className="alert alert-error">
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <span>{errorMessage}</span>
+          </div>
+        ) : null}
+        
+        {searchType === SearchTypes.Anime ? (
+          <div className='flex justify-center flex-col max-w-xl'>
+            {result.map((anime, index) => (
+              <AnimeCard key={index} anime={anime} />
+            ))}
+          </div>
+        ) : null}
+
+        {searchType === SearchTypes.WatchList ? (
+          <div className='flex justify-center flex-col px-5'>
+            <AnimeTable data={watchListResult} />
+          </div>
+        ) : null}
       </div>
     </div>
   );
