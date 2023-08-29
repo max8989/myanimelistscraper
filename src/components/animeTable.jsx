a10742799f8a2854d3bc6828f7f5a1d1c0f79f60
Maxime Gagne <max.8989@hotmail.com>
2023-08-28 20:23:47 -0400

    format

--- src/components/animeTable.jsx
+++ src/components/animeTable.jsx
@@ -1,16 +1,15 @@
-import { AnimeTableRow } from "./animeTableRow";
+import { AnimeTableRow } from './animeTableRow';
 
 export function AnimeTable({ data }) {
-    return (
-      <div className="overflow-x-auto">
-        <table className="table table-pin-rows table-pin-cols">
-          <tbody>
-            {data.map((anime, idx) => (
-              <AnimeTableRow key={idx} anime={anime} idx={idx} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-  
+  return (
+    <div className='overflow-x-auto'>
+      <table className='table table-pin-rows table-pin-cols'>
+        <tbody>
+          {data.map((anime, idx) => (
+            <AnimeTableRow key={idx} anime={anime} idx={idx} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
